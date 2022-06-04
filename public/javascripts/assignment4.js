adb03bfba6c0a7e51ab8c768af88f04a9236aade
myhblaw <105828460+myhblaw@users.noreply.github.com>
2022-06-03 23:21:01 -0400

    Assignment 1

--- public/javascripts/assignment4.js
+++ public/javascripts/assignment4.js
@@ -14,31 +14,18 @@ function createEventListner() {
 function clearForm() {
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
-    document.getElementById('address').value = '';
-    document.getElementById('city').value = '';
-    document.getElementById('postalCode').value = '';
-    document.getElementById('province').value = '';
-    document.getElementById('age').value = '';
-    document.getElementById('password').value = '';
-    document.getElementById('confirm').value = '';
+    document.getElementById('phone').value = '';
     document.getElementById('mail').value = '';
     }
 
 function formValidation() {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
-    var address = document.getElementById('address').value;
-    var city = document.getElementById('city').value;
-    var postalCode = document.getElementById('postalCode').value;
-    var province = document.getElementById('province').value;
-    var age = document.getElementById('age').value;
-    var password = document.getElementById('password').value;
-    var confirm = document.getElementById('confirm').value;
+    var address = document.getElementById('phone').value;
     var email = document.getElementById('mail').value;
     var testResult = true;
     var emailExpression = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/; // regular expression for validating email
-    var passwordExpression = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/; //regular expression for validating password
-    var postalExpression = /^[A-Za-z]\d[A-Za-z][-]?\d[A-Zaz]\d/; // regular expression for a0a0a0 format
+
 
     if (firstName == "") {
     document.getElementById('fnameError').innerHTML = "Please enter the First Name";
@@ -58,88 +45,13 @@ function formValidation() {
     document.getElementById('lnameError').innerHTML = "";
     }
 
-    if (address == "") {
-    document.getElementById('addressError').innerHTML = "Please enter a valid Address.";
+    if (phone == "") {
+    document.getElementById('phoneError').innerHTML = "Please enter a valid phone number.";
     testResult = false;
     }
     
     else {
-    document.getElementById('addressError').innerHTML = "";
-    }
-
-    if (city == "") {
-    document.getElementById('cityError').innerHTML = "Please enter a valid City.";
-    testResult = false;
-    } 
-    
-    else {
-    document.getElementById('cityError').innerHTML = "";
-    }
-
-    if (postalCode == "") {
-    document.getElementById('postalCodeError').innerHTML = "Please enter a valid Postal Code.";
-    testResult = false;
-    }
-    
-    else if (!postalExpression.test(postalCode)) {
-    alert("Postal Code should be in a0a0a0 format.");
-    testResult = false;
-    } 
-    
-    else {
-    document.getElementById('postalCodeError').innerHTML =
-    "";
-    }
-
-    if (province == "") {
-    document.getElementById('provinceError').innerHTML = "Please choose a valid Province.";
-    testResult = false;
-    }
-    
-    else {
-    document.getElementById('provinceError').innerHTML = "";
-    }
-
-    if (age == "") {
-    document.getElementById('ageError').innerHTML = "Please enter a valid Age.";
-    testResult = false;
-    }
-
-    else if (age < 18) {
-    document.getElementById('ageError').innerHTML = "Age should be atleast 18 years.";
-    testResult = false;
-    }
-    
-    else {
-    document.getElementById('ageError').innerHTML = "";
-    }
-
-    if (password == "") {
-    document.getElementById('passwordError').innerHTML = "Please enter a valid Password.";
-    testResult = false;
-    } 
-
-    else if (!passwordExpression.test(password)) {
-    alert("Passwords must have at least 6 characters and must contain at least one digit and one upper-case character.");
-    testResult = false;
-    }
-    
-    else {
-    document.getElementById('passwordError').innerHTML = "";
-    }
-
-    if (confirm == "") {
-    document.getElementById('confirmError').innerHTML = "Please enter the same Password";
-    testResult = false;
-    } 
-
-    else if (confirm != password) {
-    document.getElementById('confirmError').innerHTML = "Passwords must be identical.";
-    testResult = false;
-    }
-    
-    else {
-    document.getElementById('confirmError').innerHTML = "";
+    document.getElementById('phoneError').innerHTML = "";
     }
 
     if (email == "") {
